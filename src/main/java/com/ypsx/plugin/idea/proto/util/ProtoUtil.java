b2d85fe57e5594303a97524a02c62802e92848aa
abcCL <abccl@whu.edu.cn>
2021-07-10 11:27:33 +0800

    彻底修复重名问题导致的bug

--- src/main/java/com/ypsx/plugin/idea/proto/util/ProtoUtil.java
+++ src/main/java/com/ypsx/plugin/idea/proto/util/ProtoUtil.java
@@ -11,41 +11,45 @@ import io.protostuff.jetbrains.plugin.psi.ProtoPsiFileRoot;
 import io.protostuff.jetbrains.plugin.psi.ProtoType;
 import io.protostuff.jetbrains.plugin.psi.RpcMethodNode;
 import io.protostuff.jetbrains.plugin.psi.ServiceNode;
+import org.apache.commons.collections.CollectionUtils;
 
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class ProtoUtil {
 
     /**
      * Searches the entire project for Protobuf language files with instances of the PbServiceMethod name with the given methodName.
      *
-     * @param project current project
-     * @param methodName     to check
+     * @param project    current project
+     * @param methodName to check
      * @return matching properties
      */
-    public static List<RpcMethodNode> findProperties(Project project, String methodName,String className) {
+    public static List<RpcMethodNode> findProperties(Project project, String methodName, String classFullName) {
         List<RpcMethodNode> result = new ArrayList<>();
         Collection<VirtualFile> virtualFiles =
                 FileTypeIndex.getFiles(ProtoFileType.INSTANCE, GlobalSearchScope.projectScope(project));
-        for (VirtualFile virtualFile : virtualFiles) {
-            ProtoPsiFileRoot protoFile = (ProtoPsiFileRoot) PsiManager.getInstance(project).findFile(virtualFile);
-            Collection<ProtoType> declaredTypes = protoFile.getProtoRoot().getDeclaredTypes();
-            Optional<ProtoType> serviceNode = declaredTypes.stream().filter(protoType -> protoType instanceof ServiceNode).findAny();
-            if (serviceNode.isPresent()) {
-                ServiceNode node = (ServiceNode) serviceNode.get();
-                List<RpcMethodNode> rpcMethods = node.getRpcMethods();
-                for (RpcMethodNode methodNode : rpcMethods) {
-                    if (methodNode.getMethodName().equals(methodName) && className.startsWith(node.getName())) {
-                        result.add(methodNode);
-                    }
-                }
-                 rpcMethods.stream().filter(method -> method.getMethodName().equals(methodName)).findAny();
 
-            }
-        }
+        virtualFiles.stream()
+                .map(file -> (ProtoPsiFileRoot) PsiManager.getInstance(project).findFile(file))
+                .filter(file -> classFullName.startsWith(file.getPackageName()))
+                .forEach(file -> {
+                    Collection<ProtoType> declaredTypes = file.getProtoRoot().getDeclaredTypes();
+                    Optional<ProtoType> serviceNode = declaredTypes.stream().filter(protoType -> protoType instanceof ServiceNode).findAny();
+                    if (serviceNode.isPresent()) {
+                        ServiceNode node = (ServiceNode) serviceNode.get();
+                        List<RpcMethodNode> rpcMethods = node.getRpcMethods();
+                        for (RpcMethodNode methodNode : rpcMethods) {
+                            if (methodNode.getMethodName().equals(methodName) && classFullName.startsWith(file.getPackageName() + "." + node.getName())) {
+                                result.add(methodNode);
+                            }
+                        }
+                        rpcMethods.stream().filter(method -> method.getMethodName().equals(methodName)).findAny();
+                    }
+                });
         return result;
     }
 }
